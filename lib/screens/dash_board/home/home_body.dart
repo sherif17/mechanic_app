a4189b051f2fb67bf64d40da8ea83f72263f045f
sherif17 <sherif17.ahmed@gmail.com>
2021-06-05 17:17:17 +0200

    preparing for adding vero's part in the project.

--- lib/screens/dash_board/home/home_body.dart
+++ lib/screens/dash_board/home/home_body.dart
@@ -12,57 +12,9 @@ class HomeBody extends StatefulWidget {
 }
 
 class _HomeBodyState extends State<HomeBody> {
-  Completer<GoogleMapController> _completerGoogleMap = Completer(); //////
-  GoogleMapController _googleMapController;
-  Position currentPosition;
-  var geoLocator = Geolocator();
-
-  void locatePosition(context) async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    currentPosition = position;
-    print("Current position:: $currentPosition");
-    LatLng latLatPosition = LatLng(position.latitude, position.longitude);
-
-    CameraPosition cameraPosition =
-        new CameraPosition(target: latLatPosition, zoom: 15.5);
-    _googleMapController
-        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-  }
-
-  bool buttonState = true;
-
-  final CameraPosition _initialPosition = CameraPosition(
-    target: LatLng(31.2001, 29.9187),
-    zoom: 15.4746,
-  );
-
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(
-          top: size.height * 0.03,
-        ),
-        child: Container(
-          height: size.height * 0.92,
-          child: GoogleMap(
-            initialCameraPosition: _initialPosition,
-            mapType: MapType.normal,
-            myLocationButtonEnabled: true,
-            myLocationEnabled: true,
-            zoomGesturesEnabled: true,
-            zoomControlsEnabled: true,
-            mapToolbarEnabled: true,
-            onMapCreated: (GoogleMapController controller) {
-              _completerGoogleMap.complete(controller);
-              _googleMapController = controller;
-              locatePosition(context);
-            },
-          ),
-        ),
-      ),
-    );
+    // TODO: implement build
+    throw Container();
   }
 }
